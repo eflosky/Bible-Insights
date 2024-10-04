e81a39bacfbe450fb0ba1b420c25da64ebca0c3d
Ethan Flosky <eflosky@umich.edu>
2024-10-04 14:37:17 -0400

    Setup frontend development

---
+++ client/src/App.test.tsx
@@ -0,0 +1,10 @@
+import {expect, it, describe, beforeEach} from "vitest";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App Tests', () => {
+    it('Should be a test', () => {
+        expect(true);
+    });
+});
