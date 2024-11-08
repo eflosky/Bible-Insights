59e7dd9a4856d0525733c5e9e0d93bc5d4abc15b
Ethan Flosky <eflosky@umich.edu>
2024-11-08 14:15:15 -0500

    implemented banner

--- client/src/Components/Banner.test.tsx
+++ client/src/Components/Banner.test.tsx
@@ -8,7 +8,13 @@ describe('Header Tests', () => {
         render(<Banner />);
     });
 
-    it('Expect logo to appear', () => {
-        screen.getByTestId("Banner-Container")
+    it('Expect Bible Notes info to appear', () => {
+        screen.getByText('Bible Notes Tracker');
+        screen.getByText('Gain Insights into your Personal Study');
+        screen.getByText('Track Now!');
+    });
+
+    it('Expect Bible Notes data to appear', () => {
+        screen.getByAltText('Data diagram');
     });
 });
--- client/src/Components/Banner.tsx
+++ client/src/Components/Banner.tsx
@@ -1,9 +1,19 @@
 import './Banner.scss'
+import dataDiagram from '../Assets/DataDiagram.png'
 
-export default function Banner(){
+export default function Banner() {
     return (
         <div className={"Banner"} alt-text={'Banner'}>
-            <div className={"Banner-Container"} data-testId={"Banner-Container"}>
+            <div className={"Banner-Container"}>
+                <div className={"info"}>
+                    <h1>Bible Notes Tracker</h1>
+                    <p>Gain Insights into your Personal Study</p>
+                    <div className={"track-now"}>Track Now!</div>
+                </div>
+
+                <div className={"data"}>
+                    <img src={dataDiagram} alt={'Data diagram'} />
+                </div>
             </div>
         </div>
     )
