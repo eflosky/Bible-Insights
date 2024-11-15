1e5dcf2d6a8cf974239b382020a07a7a76ba8f06
Ethan Flosky <eflosky@umich.edu>
2024-11-15 14:32:09 -0500

    We Believe Section complete
    completed skeleton of we believe section

--- client/src/Components/WeBelieve.test.tsx
+++ client/src/Components/WeBelieve.test.tsx
@@ -12,6 +12,17 @@ describe('We Believe Tests', () => {
        const circle1 = screen.getAllByRole('img')[0] as HTMLImageElement;
        circle1.src = 'Circle.svg';
        screen.getByText('What books are in the Bible?');
-       screen.getByText('Find out here!');
+       screen.getAllByText('Find out here!')[0];
     });
+
+    it('Expect What is each book about in the Bible?', () => {
+        const circle1 = screen.getAllByRole('img')[0] as HTMLImageElement;
+        circle1.src = 'Circle.svg';
+        screen.getByText('What is each book about in the Bible?');
+        screen.getAllByText('Find out here!')[1];
+    });
+
+    it('Expect we believe heading to appear', () => {
+        screen.getByRole('heading', {name: 'We Believe ALL of the Bible is God’s inspired word and should be read.'});
+    })
 });
--- client/src/Components/WeBelieve.tsx
+++ client/src/Components/WeBelieve.tsx
@@ -5,11 +5,24 @@ import circle from '../Assets/Circle.svg'
 export default function WeBelieve() {
     return (
         <div className={"WeBelieve"} alt-text={'We Believe Section'}>
+
             <div className={"QuestionAndLink"}>
                 <img src={circle}/>
-                <p>What books are in the Bible?</p>
-                <div className={'LinkToPage'}>Find out here!</div>
+                <div className={'content'}>
+                    <p>What books are in the Bible?</p>
+                    <div className={'linkToPage'}>Find out here!</div>
+                </div>
             </div>
+
+            <div className={"QuestionAndLink"}>
+                <img src={circle}/>
+                <div className={'content'}>
+                    <p>What is each book about in the Bible?</p>
+                    <div className={'linkToPage'}>Find out here!</div>
+                </div>
+            </div>
+
+            <h1>We Believe ALL of the Bible is God’s inspired word and should be read.</h1>
         </div>
     )
 }
